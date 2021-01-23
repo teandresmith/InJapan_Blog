d53fbbf7db48a5a6b0e84c1c0f2f53cc9bf097f9
Te'Andre Smith <teandre3@hotmail.com>
2021-01-24 01:42:33 +0900

    db

--- api/models/User_Models/user_model.py
+++ api/models/User_Models/user_model.py
@@ -3,11 +3,11 @@ from flask_login import UserMixin
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'adminlist'
+    __tablename__ = 'admintable'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(200))
 
-    def __init__(self, username, password):
-        self.username = username
+    def __init__(self, name, password):
+        self.name = name
         self.password = password
